08c02a5d44f8ff98c805617acc93315cf73913ea
Evelyn <evieb16@gmail.com>
2015-12-18 14:31:19 -0800

    Adds style to shopping list page

--- shopping-list.js
+++ shopping-list.js
@@ -5,7 +5,7 @@ window.onload = function () {
   var ul = document.createElement("ul");
 
   list.appendChild(ul);
-  
+
   add.addEventListener("click", function () {
     event.preventDefault();
     // set up list element which contains text, complete btn, delete btn
@@ -14,46 +14,56 @@ window.onload = function () {
     // set up text element
     var text = document.createElement("p");
     var value = item.value;
-    text.innerHTML = value;
-    listElement.appendChild(text);
+    if (value.length !== 0) {
+      text.innerHTML = value;
+      listElement.appendChild(text);
 
-    // set up complete button
-    var completed = document.createElement("button");
-    completed.innerHTML = "Completed";
-    completed.addEventListener("click", function () {
-      event.preventDefault();
-      console.log("completed");
-      if (this.innerHTML === "Completed") {
-        ul.removeChild(listElement);
-        ul.appendChild(listElement);
-        text.style.color = "gray";
-        text.innerHTML = value.strike();
-        this.innerHTML = "Incomplete";
-      } else {
-        ul.insertBefore(listElement, ul.firstChild);
-        text.style.color = "black";
-        text.innerHTML = value;
-        this.innerHTML = "Completed";
-      }
-    });
-    listElement.appendChild(completed);
+      // set up complete button
+      var completed = document.createElement("button");
+      completed.innerHTML = "✓";
+      completed.id = "completed";
+      completed.addEventListener("click", function () {
+        event.preventDefault();
+        console.log("completed");
+        if (this.innerHTML === "✓") {
+          ul.removeChild(listElement);
+          ul.appendChild(listElement);
+          text.style.color = "rgb(26, 237, 155)";
+          text.style.backgroundColor = "rgba(42, 37, 40, 0.48)";
+          text.innerHTML = value.strike();
+          this.innerHTML = "Incomplete";
+        } else {
+          ul.insertBefore(listElement, ul.firstChild);
+          text.style.color = "rgba(1, 11, 13, 0.82)";
+          text.style.backgroundColor = "rgba(26, 237, 155, .7)";
+          text.innerHTML = value;
+          this.innerHTML = "✓";
+        }
+      });
+      listElement.appendChild(completed);
 
-    // set up delete button
-    var erase = document.createElement("button");
-    erase.innerHTML = "X";
-    erase.addEventListener("click", function () {
-      event.preventDefault();
-      console.log("delete");
-      if (erase.innerHTML === "Delete") {
-        ul.removeChild(listElement);
-      } else {
-        erase.innerHTML = "Delete";
-      }
-    });
-    listElement.appendChild(erase);
+      // set up delete button
+      var erase = document.createElement("button");
+      erase.innerHTML = "✘";
+      erase.value = "✘";
+      erase.style.color = "rgb(240, 56, 46)";
+      erase.addEventListener("click", function () {
+        event.preventDefault();
+        console.log("delete");
+        if (erase.innerHTML === "Delete") {
+          ul.removeChild(listElement);
+        } else {
+          erase.innerHTML = "Delete";
+        }
+      });
+      listElement.appendChild(erase);
 
-    ul.insertBefore(listElement, ul.firstChild);
+      ul.insertBefore(listElement, ul.firstChild);
 
-    item.value = "";
+      item.value = "";
+    } else {
+      // ask for input
+    }
+
   });
 };
