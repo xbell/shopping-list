036ed111f4d02224aaabc83a665f64ce24ef830a
Evelyn <evieb16@gmail.com>
2015-12-17 21:23:34 -0800

    Adds complete and delete buttons

--- shopping-list.js
+++ shopping-list.js
@@ -1,39 +1,67 @@
-// var listArr = function (item) {
-//   var newListArr = []
-//   newListArr.push(item);
-// }
-
-
-
 window.onload = function () {
   var list = document.getElementById("list");
   var item = document.getElementById("item");
-  var addedItem = document.getElementById("added-item");
+  var add = document.getElementById("add");
+  var ul = document.createElement("ul");
 
-  var li;
-  // item = list.item.value;
+
+
+  // function add() {
+      // var completed = document.createElement("button");
+      // completed.setAttribute("value", "Completed");
+      // element.setAttribute("name", "button3");
+      // element.setAttribute("onclick", "foo()");
+      // document.flotta.appendChild(element);
+  // }
 
   // var turnGray = function () {
   //   this.style.color = "gray";
   // }
 
-  list.addEventListener("submit", function (event) {
+  add.addEventListener("click", function () {
     event.preventDefault();
-    unorderedListElement = document.createElement("ul")
-    listElement = document.createElement("li");
+    var listElement = document.createElement("li");
     listElement.innerHTML = item.value;
-    list.appendChild(unorderedListElement);
-    unorderedListElement.appendChild(listElement)
+    // set up complete button
+    var completed = document.createElement("button");
+    completed.innerHTML = "Completed";
+    completed.addEventListener("click", function () {
+      event.preventDefault();
+      console.log("completed");
+      ul.removeChild(listElement);
+      ul.appendChild(listElement);
+    });
+    listElement.appendChild(completed);
+    // set up delete button
+    var erase = document.createElement("button");
+    erase.innerHTML = "X";
+    erase.addEventListener("click", function () {
+      event.preventDefault();
+      console.log("delete");
+      if (erase.innerHTML === "Delete") {
+        ul.removeChild(listElement);
+      } else {
+        erase.innerHTML = "Delete";
+      }
+
+
+    });
+    listElement.appendChild(erase);
+
+    list.appendChild(ul);
+    ul.appendChild(listElement);
     item.value = "";
+
+    // listElement.document.getElement
     // listElement.addEventListener("click", turnGray)
 
-    listElement.addEventListener("click", function () {
-      this.parentNode.removeChild(this);
-    });
+    // listElement.addEventListener("click", function () {
+    //   this.parentNode.removeChild(this);
+    // });
 
 
   });
 
 
 
-}
+};
